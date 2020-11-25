77cbc65ba20332e07e2199a885deb4f5f8bcf152
Nantian <nxiiun@gmail.com>
2020-11-25 21:20:26 +1300

    update

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # acala-evm-tests
 
 Run tests: `yarn workspace linkdrop run test "**/*.ts"`
+
+## run scanner 
+
+### start db
+
+```bash
+$ cd packages/indexer
+$ docker-compose up
+```
+
+### remove db
+
+```bash
+$ cd packages/indexer
+$ docker-compose stop && docker-compose rm
+```
+
+### start indexer
+
+```bash
+$ yarn workspace indexer run start
+```
+
+
--- packages/indexer/index.ts
+++ packages/indexer/index.ts
@@ -2,8 +2,8 @@ import Indexer from "@open-web3/indexer";
 import { types, typesBundle } from "@acala-network/type-definitions"
 
 const run = async () => {
-  const dbUrl = "postgres://postgres:postgres@192.168.1.10:5432/postgres";
-  const wsUrl = "ws://192.168.1.10:9944";
+  const dbUrl = "postgres://postgres:postgres@127.0.0.1:5432/postgres";
+  const wsUrl = "ws://127.0.0.1:9944";
 
   const indexer = await Indexer.create({
     dbUrl,
