4d588906cd860d2971c8b4e7164c1099e40d3108
Nantian <nxiiun@gmail.com>
2020-11-25 21:09:24 +1300

    update

---
+++ packages/indexer/index.ts
@@ -0,0 +1,67 @@
+import Indexer from "@open-web3/indexer";
+import { types, typesBundle } from "@acala-network/type-definitions"
+
+const run = async () => {
+  const dbUrl = "postgres://postgres:postgres@192.168.1.10:5432/postgres";
+  const wsUrl = "ws://192.168.1.10:9944";
+
+  const indexer = await Indexer.create({
+    dbUrl,
+    wsUrl,
+    types: {
+      'EvmAddress': 'H160',
+      ExitReason: {
+        _enum: {
+          Succeed: 'ExitSucceed',
+          Error: 'ExitError',
+          Revert: 'ExitRevert',
+          Fatal: 'ExitFatal',
+        }
+      },
+      ExitSucceed: {
+        _enum: ['Stopped', 'Returned', 'Suicided']
+      },
+      ExitError: {
+        _enum: {
+          StackUnderflow: 'Null',
+          StackOverflow: 'Null',
+          InvalidJump: 'Null',
+          InvalidRange: 'Null',
+          DesignatedInvalid: 'Null',
+          CallTooDeep: 'Null',
+          CreateCollision: 'Null',
+          CreateContractLimit: 'Null',
+          OutOfOffset: 'Null',
+          OutOfGas: 'Null',
+          OutOfFund: 'Null',
+          PCUnderflow: 'Null',
+          CreateEmpty: 'Null',
+          Other: 'Text',
+        }
+      },
+      ExitRevert: {
+        _enum: ['Reverted']
+      },
+      ExitFatal: {
+        _enum: {
+          NotSupported: 'Null',
+          UnhandledInterrupt: 'Null',
+          CallErrorAsFatal: 'ExitError',
+          Other: 'Text',
+        }
+      },
+      ...types,
+    },
+    typesBundle,
+    sync: true,
+  });
+
+  await indexer.start({
+    concurrent: 1,
+    confirmation: 1
+  });
+};
+
+run().catch((err) => {
+  console.error(err);
+});
