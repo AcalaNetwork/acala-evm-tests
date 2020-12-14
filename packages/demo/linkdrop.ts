e8922c8fe911e75ac20a3befc440cd7a8c781b60
Xiliang Chen <xlchen1291@gmail.com>
2020-12-14 14:44:06 +1300

    update linkdrop demo (#3)
    * update linkdrop demo
    
    * update linkdrop
    
    * bump acala.js
    
    * add types
    
    * update types
    
    * update deps
    
    * add some logs
    
    Co-authored-by: Nantian <nxiiun@gmail.com>

--- packages/demo/linkdrop.ts
+++ packages/demo/linkdrop.ts
@@ -1,90 +1,159 @@
 import LinkdropFactory from "linkdrop/build/LinkdropFactory.json";
 import LinkdropMastercopy from "linkdrop/build/LinkdropMastercopy.json";
+import TokenMock from "linkdrop/build/TokenMock.json";
 import ERC20 from "linkdrop/build/ERC20.json";
 
-import { ethers } from "ethers";
+import { ethers, ContractFactory, Signer } from "ethers";
 
 import { createWallet, getProvider, initAccount } from "common";
 import {
   computeProxyAddress,
   createLink,
   signReceiverAddress,
-  computeBytecode,
 } from "linkdrop/scripts/utils";
 
-const DOT = '0x0000000000000000000000000000000000000802'
-const chainId = 595
-const initcode = "0x6352c7420d6000526103ff60206004601c335afa6040516060f3"
+const DOT = "0x0000000000000000000000000000000000000802";
+const chainId = 595;
+const initcode = "0x6352c7420d6000526103ff60206004601c335afa6040516060f3";
+
+const deployContract = (signer: Signer, contract: any, ...args: any[]) => {
+  return ContractFactory.fromSolidity(contract)
+    .connect(signer)
+    .deploy(...args);
+};
 
 const main = async () => {
-  const provider = getProvider()
+  const provider = getProvider();
 
-  const wallets = createWallet(provider) as any
+  const wallets = createWallet(provider) as any;
 
-  await initAccount(provider, wallets)
+  await initAccount(provider, wallets);
 
-  let [linkdropMaster, linkdropSigner, receiverAddress] = wallets
+  let [linkdropMaster, linkdropSigner, receiver, relayer] = wallets;
 
+  const campaignId = 0;
+
+  // const tokenInstance = await deployContract(linkdropMaster, TokenMock);
   const tokenInstance = new ethers.Contract(
     DOT,
     ERC20.abi,
     linkdropMaster
   )
 
-  const bal = await tokenInstance.balanceOf(linkdropMaster.address)
-  console.log(bal.toString())
-
-  const masterCopy = await deployContract(linkdropMaster, LinkdropMastercopy, [], {
-    gasLimit: 300_000_000,
+  const masterCopy = await deployContract(linkdropMaster, LinkdropMastercopy, {
+    gasLimit: 3_000_000_000,
   });
 
-  const bytecode = computeBytecode(masterCopy.address);
   const factory = await deployContract(
     linkdropMaster,
     LinkdropFactory,
-    [masterCopy.address, chainId],
+    masterCopy.address,
+    chainId,
     {
-      gasLimit: 300_000_000,
+      gasLimit: 3_000_000_000,
     }
   );
 
-  const campaignId = 0;
-
-  // Compute next address with js function
-  let expectedAddress = computeProxyAddress(
-    factory.address,
-    linkdropMaster.address,
-    campaignId,
-    initcode
-  )
-
-  factory.deployProxyWithSigner(campaignId, linkdropSigner.address, {
-    gasLimit: 300_000_000,
-  })
+  await factory.deployProxy(campaignId, {
+    gasLimit: 3_000_000_000,
+  });
 
   const proxy = new ethers.Contract(
-    expectedAddress,
+    computeProxyAddress(
+      factory.address,
+      linkdropMaster.address,
+      campaignId,
+      initcode
+    ),
     LinkdropMastercopy.abi,
     linkdropMaster
-  )
+  );
+
+  await proxy.addSigner(linkdropSigner.address, { gasLimit: 3_000_000_000 });
+
+  await linkdropMaster.sendTransaction({
+    to: proxy.address,
+    value: ethers.utils.parseEther("2"),
+  });
+
+  //////////////////////
+
+  const weiAmount = 0;
+  const tokenAddress = tokenInstance.address;
+  const tokenAmount = 999;
+  const expirationTime = 100000;
+  const version = 1;
+
+  await tokenInstance.approve(proxy.address, tokenAmount);
 
   const link = await createLink(
     linkdropSigner,
-    0,
-    tokenInstance.address,
-    ethers.utils.parseEther('2'),
-    1000,
-    1,
+    weiAmount,
+    tokenAddress,
+    tokenAmount,
+    expirationTime,
+    version,
     chainId,
     proxy.address
   );
 
-  console.log(link)
-
   const receiverSignature = await signReceiverAddress(
     link.linkKey,
-    receiverAddress
+    receiver.address
   );
-}
 
-main().then(() => process.exit(0))
+  const receiverTokenBalanceBefore = await tokenInstance.balanceOf(
+    receiver.address
+  );
+
+  const approverBalanceBefore = await tokenInstance.balanceOf(
+    linkdropMaster.address
+  );
+
+  await factory.addRelayer(relayer.address);
+
+  const factoryWithRelayer = factory.connect(relayer);
+
+  console.log({
+    linkdropMaster: linkdropMaster.address,
+    linkdropSigner: linkdropSigner.address,
+    receiver: receiver.address,
+    relayer: relayer.address,
+    tokenInstance: tokenInstance.address,
+    masterCopy: masterCopy.address,
+    factory: factory.address,
+    proxy: proxy.address,
+    link
+  })
+
+  await factoryWithRelayer.claim(
+    weiAmount,
+    tokenAddress,
+    tokenAmount,
+    100000,
+    link.linkId,
+    linkdropMaster.address,
+    campaignId,
+    link.linkdropSignerSignature,
+    receiver.address,
+    receiverSignature,
+    { gasLimit: 3_000_000_000 }
+  );
+
+  const approverBalanceAfter = await tokenInstance.balanceOf(
+    linkdropMaster.address
+  );
+
+  const receiverTokenBalanceAfter = await tokenInstance.balanceOf(
+    receiver.address
+  );
+
+  console.log({
+    approverBalanceBefore: approverBalanceBefore.toString(),
+    approverBalanceAfter: approverBalanceAfter.toString(),
+    receiverTokenBalanceBefore: receiverTokenBalanceBefore.toString(),
+    receiverTokenBalanceAfter: receiverTokenBalanceAfter.toString(),
+  });
+};
+
+main().then(() => process.exit(0));
--- packages/indexer/index.ts
+++ packages/indexer/index.ts
@@ -1,64 +1,26 @@
 import Indexer from "@open-web3/indexer";
-import { types, typesBundle } from "@acala-network/type-definitions"
+import { options } from "@acala-network/api";
 
 const run = async () => {
   const dbUrl = "postgres://postgres:postgres@127.0.0.1:5432/postgres";
   const wsUrl = "ws://127.0.0.1:9944";
 
+  const acalaOptions = options();
+
   const indexer = await Indexer.create({
     dbUrl,
     wsUrl,
-    types: {
-      'EvmAddress': 'H160',
-      ExitReason: {
-        _enum: {
-          Succeed: 'ExitSucceed',
-          Error: 'ExitError',
-          Revert: 'ExitRevert',
-          Fatal: 'ExitFatal',
-        }
-      },
-      ExitSucceed: {
-        _enum: ['Stopped', 'Returned', 'Suicided']
-      },
-      ExitError: {
-        _enum: {
-          StackUnderflow: 'Null',
-          StackOverflow: 'Null',
-          InvalidJump: 'Null',
-          InvalidRange: 'Null',
-          DesignatedInvalid: 'Null',
-          CallTooDeep: 'Null',
-          CreateCollision: 'Null',
-          CreateContractLimit: 'Null',
-          OutOfOffset: 'Null',
-          OutOfGas: 'Null',
-          OutOfFund: 'Null',
-          PCUnderflow: 'Null',
-          CreateEmpty: 'Null',
-          Other: 'Text',
-        }
-      },
-      ExitRevert: {
-        _enum: ['Reverted']
-      },
-      ExitFatal: {
-        _enum: {
-          NotSupported: 'Null',
-          UnhandledInterrupt: 'Null',
-          CallErrorAsFatal: 'ExitError',
-          Other: 'Text',
-        }
-      },
-      ...types,
-    },
-    typesBundle,
+    types: acalaOptions.types as any,
+    typesAlias: acalaOptions.typesAlias,
+    typesSpec: acalaOptions.typesSpec as any,
+    typesChain: acalaOptions.typesChain as any,
+    typesBundle: acalaOptions.typesBundle as any,
     sync: true,
   });
 
   await indexer.start({
     concurrent: 1,
-    confirmation: 1
+    confirmation: 1,
   });
 };
 
